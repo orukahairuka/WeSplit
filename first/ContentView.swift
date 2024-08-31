bb5ed4a0a283ee7c291be3ae5ba478fb55865753
irukahaoruka <erika03301109@gmail.com>
2024-09-01 00:01:54 +0900

    成長しました

--- first/ContentView.swift
+++ first/ContentView.swift
@@ -8,17 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+     @State private var money: Int = 0
+    @State private var members: [String] = []
+    @State private var addMember: String = ""
+    
+    
+    private func Calculate (members: [String], money: Int) -> Int{
+        var memberCount: Int
+        var totalMoney: Int
+        memberCount = members.count
+        if memberCount != 0{
+            totalMoney = money/memberCount
+        } else {
+            totalMoney = 0
+        }
+        return totalMoney
+    }
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Form {
+                TextField("money", value: $money, format: .number)
+            }
+            Form {
+                TextField("member", text: $addMember)
+            }
+            Button {
+                if addMember != ""{
+                    members.append(addMember)
+                    addMember = ""
+                } else {
+                    print("新メンバーを入力して")
+                }
+            } label: {
+                Text("新メンバーを加える")
+            }
+            
+            List {
+                ForEach(members, id: \.self) { member in
+                    Text(member)
+                }
+            }
+            Text("\(Calculate(members: members, money: money))円で分けてね")
         }
         .padding()
     }
 }
 
-#Preview {
-    ContentView()
-}
+
