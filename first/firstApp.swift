4958f3e1632518ef0c073090563ffee9618998dc
irukahaoruka <erika03301109@gmail.com>
2024-08-31 23:08:27 +0900

    Initial Commit

---
+++ first/firstApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstApp.swift
+//  first
+//
+//  Created by 櫻井絵理香 on 2024/08/31.
+//
+
+import SwiftUI
+
+@main
+struct firstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
